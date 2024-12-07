3bb332cefa930ff12ceb8255131685071a2a276a
ines jannedi <inesjannedi11@gmail.com>
2024-12-08 01:35:20 +0200

    .

--- travel-app/next.config.ts
+++
@@ -1,9 +0,0 @@
-const withTypescript = require('@zeit/next-typescript')({
-  typescript: {
-    tsconfigPath: './tsconfig.json',
-  },
-});
-
-module.exports = withTypescript({
-  // Your Next.js configuration
-});
